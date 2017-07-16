39610dbf5ecf7574d7d322a3050f58ff22ef7325
Kazuhiro Furue <k.furue0728@gmail.com>
2017-07-16 22:04:01 +0000

    Add phone to ticket

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170716122742) do
+ActiveRecord::Schema.define(version: 20170716125016) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170716122742) do
     t.string "email_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
